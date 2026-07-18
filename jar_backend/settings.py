530b324aa1de0709bc76eea8d867e92d68c53291
Alvaro Lopez Garcia <alopegarci@gmail.com>
2026-07-18 15:05:01 +0200

    feat(auth): migrate from Token authentication to JWT using SimpleJWT

--- jar_backend/settings.py
+++ jar_backend/settings.py
@@ -50,7 +50,7 @@ SHARED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'warehouse_management',
 )
 
@@ -60,7 +60,7 @@ TENANT_APPS = (
     'django.contrib.admin',
     'warehouse_management',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 )
 
 INSTALLED_APPS = list(SHARED_APPS) + [app for app in TENANT_APPS if app not in SHARED_APPS]
@@ -146,13 +146,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 LOGGING = {
--- jar_backend/urls.py
+++ jar_backend/urls.py
@@ -1,8 +1,8 @@
 # jar_backend/urls.py (o el nombre de tu proyecto)
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
-from tenants.views import CustomAuthTokenView
+from rest_framework_simplejwt.views import TokenRefreshView
+from tenants.views import CustomTokenObtainPairView
 
 from jar_backend.views import health_check
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/health/', health_check, name='health_check'),
-    path('api/v1/get-token/', CustomAuthTokenView.as_view(), name='api_token_auth'),
+    path('api/v1/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/warehouse/', include('warehouse_management.urls')),
 ]
